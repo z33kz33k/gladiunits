44000d3075dd68a5707a9051821e1ba6608ad04e
z33k <piotr.mazik@protonmail.com>
2023-12-23 02:12:23 +0100

    Start work on data structures (WIP)

--- gladiunits/__init__.py
+++ gladiunits/__init__.py
@@ -7,7 +7,54 @@
     @author: z33k
 
 """
+from dataclasses import dataclass
+from typing import Tuple
+
 from gladiunits.utils import init_log
 
+
 init_log()
+
+
+@dataclass
+class Weapon:
+    name: str
+
+
+@dataclass
+class Action:
+    name: str
+
+
+@dataclass
+class Trait:
+    name: str
+
+
+@dataclass
+class Upgrade:
+    name: str
+    tier: int
+
+
+@dataclass
+class Unit:
+    name: str
+    armor: int
+    hitpoints: float
+    movement: int
+    morale: int
+    melee_accuracy: int
+    melee_attacks: int
+    ranged_accuracy: int
+    ranged_attacks: int
+    production_cost: float
+    requisitions_upkeep: float
+    requisitions_cost: float
+    group_size: int
+    weapons: Tuple[Weapon, ...]
+
+    @property
+    def total_hitpoints(self) -> float:
+        return self.hitpoints * self.group_size
 
