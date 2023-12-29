4fce217fabf9c1943c0af8482863d813ca01a38c
z33k <piotr.mazik@protonmail.com>
2023-12-29 21:33:41 +0100

    Improve parsing of displayed texts (#1). Further develop data structures (WIP)

--- gladiunits/data.py
+++ gladiunits/data.py
@@ -1,17 +1,77 @@
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Tuple
 
 
+CATEGORIES = [
+    'Actions',
+    'Buildings',
+    'Features',
+    'Items',
+    'Traits',
+    'Units',
+    'Upgrades',
+    'Weapons'
+]
+FACTIONS = [
+    'AdeptusMechanicus',
+    'AstraMilitarum',
+    'ChaosSpaceMarines',
+    'Drukhari',
+    'Eldar',
+    'Necrons',
+    'Neutral',
+    'Orks',
+    'SistersOfBattle',
+    'SpaceMarines',
+    'Tau',
+    'Tyranids',
+]
+
+
 @dataclass(frozen=True)
-class Trait:
-    name: str
+class Origin:
+    path: Path
+
+    @property
+    def name(self) -> str:
+        return self.path.stem
+
+    @property
+    def category(self) -> str:
+        parent = self.path.parent
+        if parent.name in FACTIONS:
+            category = parent.parent.name
+        else:
+            category = parent.name
+            if category == "Artefacts":
+                if parent.parent.name in CATEGORIES:
+                    category = parent.parent.name
+                else:
+                    category = parent.parent.parent.name
+        return category
+
+    @property
+    def category_path(self) -> Path:
+        idx = self.path.parts.index(self.category)
+        return Path(*self.path.parts[idx:])
+
+    def __post_init__(self) -> None:
+        if self.category not in CATEGORIES:
+            raise ValueError(f"Unknown category: {self.category!r}")
+
+
+@dataclass(frozen=True)
+class Trait(Origin):
     required_upgrade: str | None
 
+    def __post_init__(self) -> None:
+        if self.category != "Traits":
+            raise ValueError(f"Not a path to a trait .xml: {self.path}")
+
 
 @dataclass(frozen=True)
-class Weapon:
-    name: str
-    displayed_name: str
+class Weapon(Origin):
     attacks: int | None
     melee_armor_penetration: int | None
     melee_damage: float | None
@@ -20,36 +80,45 @@ class Weapon:
     range: int | None
     traits: Tuple[Trait, ...]
 
+    def __post_init__(self) -> None:
+        if self.category != "Weapons":
+            raise ValueError(f"Not a path to a weapon .xml: {self.path}")
+
 
 @dataclass(frozen=True)
-class Upgrade:
-    name: str
+class Upgrade(Origin):
     tier: int
+    reference: Origin
     required_upgrades: Tuple["Upgrade", ...]
 
+    @property
+    def ref_category(self) -> str:
+        return self.reference.category
+
     def __post_init__(self) -> None:
-        if self.tier not in range(1, 11):
-            raise ValueError("Tier must be an integer between 1 and 10")
+        if self.category != "Upgrades":
+            raise ValueError(f"Not a path to an upgrade .xml: {self.path}")
+        if self.tier not in range(11):
+            raise ValueError("Tier must be an integer between 0 and 10")
         if any(u.tier > self.tier for u in self.required_upgrades):
             raise ValueError("Required upgrades cannot be of surpassing tier")
 
 
 @dataclass(frozen=True)
-class Action:
-    name: str
-    displayed_name: str
+class Action(Origin):
     weapon: Weapon | None
     cooldown: int | None
+    required_upgrade: Upgrade | None
 
     def __post__init__(self) -> None:
+        if self.category != "Units":
+            raise ValueError(f"Not a path to an unit .xml: {self.path}")
         if self.cooldown and self.cooldown < 0:
             raise ValueError("Cooldown must not be negative")
 
 
 @dataclass(frozen=True)
-class Unit:
-    name: str
-    displayed_name: str
+class Unit(Origin):
     armor: int
     hitpoints: float
     movement: int
@@ -69,5 +138,9 @@ class Unit:
     @property
     def total_hitpoints(self) -> float:
         return self.hitpoints * self.group_size
+
+    def __post__init__(self) -> None:
+        if self.category != "Units":
+            raise ValueError(f"Not a path to an unit .xml: {self.path}")
 
 
--- gladiunits/parse.py
+++ gladiunits/parse.py
@@ -1,24 +1,39 @@
 import re
+from collections import deque
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 import lxml
 from lxml.etree import XMLSyntaxError, _Element as Element
 
 from gladiunits.constants import PathLike
+from gladiunits.data import Origin, CATEGORIES
 
 
-class XmlParser:
+class FileParser:
     @property
     def file(self) -> Path:
         return self._file
 
     @property
+    def lines(self) -> List[str]:
+        with self.file.open(encoding='utf8') as f:
+            lines = [*f]
+        return lines
+
+    def __init__(self, file: PathLike) -> None:
+        self._file = Path(file)
+        if not self.file.is_file():
+            raise FileNotFoundError(f"Not a file: '{self.file}'")
+
+
+class XmlParser(FileParser):
+    @property
     def root(self) -> Element:
         return self._root
 
     def __init__(self, file: PathLike) -> None:
-        self._file = Path(file)
+        super().__init__(file)
         try:
             self._root: Element = lxml.etree.parse(self.file).getroot()
         except XMLSyntaxError as e:
@@ -56,17 +71,128 @@ class XmlParser:
         return re.sub(r"(?<=value=\")<.*>(?=\")", replace_brackets, line)
 
 
-def _parse_displayed_names(file: PathLike) -> Dict[str, str]:
-    xml = XmlParser(file)
-    entries = [entry for entry in xml.root.findall(".//entry")
-               if not entry.attrib["name"].endswith("Description")
-               and not entry.attrib["name"].endswith("Flavor")
-               and "{" not in entry.attrib["value"]]
-    return {entry.attrib["name"]: entry.attrib["value"] for entry in entries}
+class _EntryLine:
+    PATTERN_TEMPLATE = r'{}=\"(.*)\"'
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def category(self) -> str:
+        return self._category
+
+    @property
+    def name_origin(self) -> Origin:
+        path = Path(self.category) / self.name
+        return Origin(path)
+
+    @property
+    def value(self) -> str:
+        return self._value
+
+    @property
+    def ref(self) -> Origin | None:
+        return self._ref
+
+    def __init__(self, line: str, category: str) -> None:
+        if not self.is_valid(line):
+            raise ValueError(f"Invalid entry line: '{line}'")
+        self._line, self._category = line, category
+        self._ref = None
+        self._name = self._parse_name()
+        self._value = self._parse(self._line, "value")
+        if self.value.startswith("<string name="):
+            self._ref = Origin(Path(self._parse(self.value, "name", double_quotes=False)))
+
+    def __repr__(self) -> str:
+        text = f"{self.__class__.__name__}(origin='{self.name_origin.path}'"
+        if self.ref:
+            text += f", ref='{self.ref.path}')"
+        else:
+            text += f", value='{self.value}')"
+        return text
+
+    @classmethod
+    def _parse(cls, text: str, attr: str, double_quotes=True) -> str:
+        if double_quotes:
+            pattern = cls.PATTERN_TEMPLATE.format(attr)
+        else:
+            pattern = cls.PATTERN_TEMPLATE.replace('"', "'").format(attr)
+        compiled_pattern = re.compile(pattern)
+        match = compiled_pattern.search(text)
+        if not match:
+            raise ValueError(f"Cannot parse {attr!r} in '{text}'")
+        return match.group(1)
+
+    def _parse_name(self) -> str:
+        text, *_ = self._line.split("value=")
+        return self._parse(text.strip(), "name")
+
+    @staticmethod
+    def is_valid(line: str) -> bool:
+        return not any(token not in line for token in ("<entry", "name=", "value=", "/>"))
 
 
-DISPLAYED_NAMES = {
-    "actions": _parse_displayed_names(r"xml/Core/Languages/English/Actions.xml"),
-    "units": _parse_displayed_names(r"xml/Core/Languages/English/Units.xml"),
-    "weapons": _parse_displayed_names(r"xml/Core/Languages/English/Weapons.xml"),
-}
+class _CoreFileParser(FileParser):
+    @property
+    def category(self) -> str:
+        return self.file.stem
+
+    @property
+    def entry_lines(self) -> List[_EntryLine]:
+        return self._entry_lines
+
+    @property
+    def plain_lines(self) -> List[_EntryLine]:
+        return [line for line in self.entry_lines if not line.ref]
+
+    @property
+    def reffed_lines(self) -> List[_EntryLine]:
+        return [line for line in self.entry_lines if line.ref]
+
+    @property
+    def refs(self) -> List[Origin]:
+        return sorted({el.ref for el in self.reffed_lines}, key=lambda r: str(r.path))
+
+    def __init__(self, file: PathLike) -> None:
+        super().__init__(file)
+        if self.category not in CATEGORIES:
+            raise ValueError(f"Unknown category: {self.category!r}")
+        self._entry_lines = []
+        for line in self.lines:
+            if _EntryLine.is_valid(line):
+                self._entry_lines.append(_EntryLine(line, self.category))
+
+
+def _parse_displayed_texts() -> Dict[Origin, str]:
+    files = (
+        r"xml/Core/Languages/English/Actions.xml",
+        r"xml/Core/Languages/English/Buildings.xml",
+        r"xml/Core/Languages/English/Features.xml",
+        r"xml/Core/Languages/English/Items.xml",
+        r"xml/Core/Languages/English/Traits.xml",
+        r"xml/Core/Languages/English/Units.xml",
+        r"xml/Core/Languages/English/Upgrades.xml",
+        r"xml/Core/Languages/English/Weapons.xml",
+    )
+    parsers = [_CoreFileParser(file) for file in files]
+    parsers.sort(key=lambda p: len(p.reffed_lines))
+
+    context = {entry_line.name_origin: entry_line.value for parser in parsers
+               for entry_line in parser.plain_lines}
+
+    stack = [entry_line for parser in parsers for entry_line in parser.reffed_lines][::-1]
+    stack = deque(stack)
+    while stack:
+        line = stack.pop()
+        found = context.get(line.ref)
+        if found:
+            context.update({line.name_origin: found})
+        else:
+            stack.appendleft(line)
+
+    return context
+
+
+DISPLAYED_TEXTS = _parse_displayed_texts()
